f96f79af4eb6ae2b2a7581665ca3f99b89b922cd
deepwwwave <deepwwwave@gmail.com>
2021-01-02 01:19:28 +0100

    find() database

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const routes = express.Router();
-
-app.use('/api',routes)
+app.use('/api',routes);
  
 // body-parser
 routes.use(bodyParser.urlencoded({ extended: false }));
@@ -32,19 +31,28 @@ client.connect((err) => {
   if (err) {
     throw Error(err);
   }
-  const collection = client.db(process.env.DB_DATABASE).collection(process.env.DB_COLLECTION);
   !err && console.log("successfully connected to database");
+  const products = client.db(process.env.DB_DATABASE).collection(process.env.DB_COLLECTION);
 
   // perform actions on the collection object
-  client.close();
-});
+  
+  routes.get("/products", (req, res ) => {
+    products
+    .find()
+    .toArray()
+    .then((error, results) => {
+      if (error) {
+        return res.send(error) 
+      }
+      res.status(200).send({results});
+    }).catch((err) => res.send(err));
 
+  });
+
+});
 
 // routes
 routes.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-routes.get("/products", (req, res ) => {
-    res.send("liste de produits");
-});
